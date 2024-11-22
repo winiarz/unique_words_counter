c1c93840fc4647112874732164aa157c2f45c40a
Konrad Winiarski <winek353@gmail.com>
2024-11-22 15:59:11 +0100

    improve data generator

--- data_generator/main.cpp
+++ data_generator/main.cpp
@@ -2,26 +2,47 @@
 #include <string>
 #include <fstream>
 #include <cstdlib>
+#include <set>
+#include <algorithm>
+#include <random>
 using namespace std;
 
 constexpr unsigned long long minWordLen = 3;
 constexpr unsigned long long maxWordLen = 10;
 constexpr int randSeed = 123456;
 
+string generateRandomWord()
+{
+	unsigned long long newWordLen = minWordLen + rand()%(maxWordLen - minWordLen);
+
+	string newWord(newWordLen, 'a');
+
+	for(int i=0; i<newWordLen; i++)
+	{
+		newWord[i] = 'a' + rand() % ('z' - 'a');
+	}
+
+	return newWord;
+}
+
 int main(int argc, char* argv[])
 {
-	if(argc <= 2)
+	srand(randSeed);
+
+	if(argc <= 3)
 	{
-		cout << "2 arguments expected:" << endl;
+		cout << "3 arguments expected:" << endl;
 		cout << "#1 filename" << endl;
-		cout << "#2 size of generated data in Bytes" << endl;
+		cout << "#2 mode: 'random' generates random words, if provided with number n instead generates n, then generates text with n unique words" << endl;
+		cout << "#3 size of generated data in Bytes" << endl;
 		return 1;
 	}
 
 	string fileName(argv[1]);
-	string dataSizeStr(argv[2]);
+	string modeOrUniqueSize(argv[2]);
+	string dataSizeStr(argv[3]);
 	unsigned long long dataSize = 0;
-	
+
 	try
 	{
 		dataSize = stoull(dataSizeStr);
@@ -29,34 +50,67 @@ int main(int argc, char* argv[])
 	catch(...)
 	{
 		cout << "'" << dataSizeStr << "' is not correct data size" << endl;
+		return 1;
 	}
-	
 
 	fstream file(fileName, fstream::out);
+
 	unsigned long long dataGenerated = 0;
 	unsigned long long wordsGenerated = 0;
 
-	srand(randSeed);
-	while(dataGenerated < dataSize)
+	if(modeOrUniqueSize.compare("random")==0)
 	{
-		unsigned long long newWordLen = minWordLen + rand()%(maxWordLen - minWordLen);
-
-		string newWord(newWordLen, 'a');
-
-		for(int i=0; i<newWordLen; i++)
+		while(dataGenerated < dataSize)
 		{
-			newWord[i] = 'a' + rand() % ('z' - 'a');
+			
+			string newWord = generateRandomWord();
+
+			if(rand()%10 == 0)
+				file << newWord << "\n";
+			else
+				file << newWord << " ";
+
+			dataGenerated += newWord.size()+1;
+			wordsGenerated++;
+		}
+	}
+	else
+	{
+		unsigned long long uniqueSize=0;
+		try
+		{
+			uniqueSize = stoull(modeOrUniqueSize);
+		}
+		catch(...)
+		{
+			cout << "'" << modeOrUniqueSize << "' is not correct number" << endl;
 		}
 
-		if(rand()%10 == 0)
-			file << newWord << "\n";
-		else
-			file << newWord << " ";
+		set<string> randomWords;
+		while(randomWords.size() < uniqueSize)
+			randomWords.insert(generateRandomWord());
 
-		dataGenerated += newWordLen+1;
-		wordsGenerated++;
+		while(dataGenerated < dataSize)
+		{
+			vector<string> wordSample;
+			sample(randomWords.begin(),
+			       randomWords.end(),
+				   std::back_inserter(wordSample),
+                   1,
+				   std::mt19937{std::random_device{}()});
+
+			if(rand()%10 == 0)
+				file << *wordSample.begin() << "\n";
+			else
+				file << *wordSample.begin() << " ";
+
+			dataGenerated += wordSample.begin()->size() + 1;
+			wordsGenerated++;
+		}
 	}
+
 	file.close();
+
 
 	cout << "Generated " << wordsGenerated << " with total length (including whitespaces) of " << dataGenerated << endl;
 
